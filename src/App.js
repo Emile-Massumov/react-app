e248a54b3c85e753d16239496c12eafb16e5f246
Émile <85419994+Emile-Massumov@users.noreply.github.com>
2022-01-23 11:15:29 -0500

    added style for my new learning React app

--- src/App.js
+++ src/App.js
@@ -1,139 +1,21 @@
-import React , {Component} from "react";
-import "./App.css";
+import React from "react";
+import Tweet from "./Tweet";
 
-class  App extends Component {
-  constructor(props) {
-    super(props);
-    this.state= {
-      newItem:"",
-      list:[]
-    }
-  }
-updateInput(key,value) {
-  //update react state
-  this.setState({
-    [key]:value
-  });
-}
-  addItem(){
-//create item with unique id 
-const newItem ={ 
-  id: 1 + Math.random (),
-  value: this.state.newItem.slice()
-};
 
-//copy of current list of items
-const list = [...this.state.list];
-//add new item to list 
-list.push(newItem);
-//update state with new list aand reset newItem input 
-this.setState ({
-  list,
-  newItem:""
-});
-
-  }
-deleteItem (id) { 
-//copy of current list of items
-const list = [...this.state.list];
-//filter out item being deleted
-const updatedList = list.filter(item => item.id !==id)
-this.setState ({ list: updatedList});
-}
-  //incorporating local storage 
-  componentDidMount() {
-    this.hydrateStateWithLocalStorage();
-
-    // add event listener to save state to localStorage
-    // when user leaves/refreshes the page
-    window.addEventListener(
-      "beforeunload",
-      this.saveStateToLocalStorage.bind(this)
-    );
-  }
-
-  componentWillUnmount() {
-    window.removeEventListener(
-      "beforeunload",
-      this.saveStateToLocalStorage.bind(this)
-    );
-
-    // saves if component has a chance to unmount
-    this.saveStateToLocalStorage();
-  }
-
-  hydrateStateWithLocalStorage() {
-    // for all items in state
-    for (let key in this.state) {
-      // if the key exists in localStorage
-      if (localStorage.hasOwnProperty(key)) {
-        // get the key's value from localStorage
-        let value = localStorage.getItem(key);
-
-        // parse the localStorage string and setState
-        try {
-          value = JSON.parse(value);
-          this.setState({ [key]: value });
-        } catch (e) {
-          // handle empty string
-          this.setState({ [key]: value });
-        }
-      }
-    }
-  }
-
-  saveStateToLocalStorage() {
-    // for every item in React state
-    for (let key in this.state) {
-      // save to localStorage
-      localStorage.setItem(key, JSON.stringify(this.state[key]));
-    }
-  }
-
-  render() { 
+function App () {
+  
   return (
-    <div App>
-    <div className="container">
-     
-          
-    Add an Item ...
-    <br/>
-    <input 
-    type="text"
-    placeholder="Type item here..."
-    value={this.state.newItem}
-    onChange={e => this.updateInput ("newItem",e.target.value)}
-    />
-    <button  className="add-btn btn-floating"
-    onClick={() => this.addItem ()}
-    disabled={!this.state.newItem.length}
-    >
-      Add
-    </button>
-    <br/>
-    <br/>
-    <ul >
-      {this.state.list.map(item => {
-        return ( 
-          <li key={item.id} >
-            {item.value}
-            <button className="addbutton"
-            onClick={() => this.deleteItem (item.id)}
-            >
-            X  
-            </button>
-    </li>
-        );
-      })}
-        </ul>   
-       </div> 
-     </div>
-    
-      )
-        }
-        }
+    <div className="app">
+      <h1>Bonjour React</h1>
+      <Tweet/>
+      <Tweet/>
+      <Tweet/>
+      </div>
+  );
+  }
+ 
         
-        export default App;
+export default App;
 
 
 
